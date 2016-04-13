b39d2f3edf5de5ff0c8434d46cff62e4c8e88fd1
Mark Jones <mark@getkahoot.com>
2016-04-13 21:08:04 +0100

    Remove data.x deps and instead use ramda-fantasy for Maybe, Either and Future

--- config.js
+++ config.js
@@ -14,11 +14,9 @@ System.config({
   },
 
   map: {
-    "data.either": "npm:data.either@1.4.0",
-    "data.maybe": "npm:data.maybe@1.2.2",
-    "data.task": "npm:data.task@3.1.0",
     "pointfree-fantasy": "npm:pointfree-fantasy@0.1.3",
     "ramda": "npm:ramda@0.21.0",
+    "ramda-fantasy": "npm:ramda-fantasy@0.4.1",
     "typescript": "npm:typescript@1.8.10",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -34,21 +32,6 @@ System.config({
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
-    },
-    "npm:data.either@1.4.0": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:data.maybe@1.2.2": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:data.task@3.1.0": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
@@ -67,6 +50,12 @@ System.config({
     },
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:ramda-fantasy@0.4.1": {
+      "ramda": "npm:ramda@0.17.1"
+    },
+    "npm:ramda@0.17.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:ramda@0.21.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
