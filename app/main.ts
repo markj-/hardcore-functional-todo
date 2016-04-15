b18ab5609ce7110b5efe5c8434ab7ace6684367a
Mark Jones <mark@getkahoot.com>
2016-04-15 13:42:39 +0100

    Create and use runIO util

--- app/main.ts
+++ app/main.ts
@@ -15,6 +15,7 @@ import {
   subscribe,
   listen
 } from 'utils/streams';
+import runIO from 'utils/run-io';
 
 const buttonClickStream = compose(map(map(listen('click'))), map(map(head)), cgetDom('button'));
 
@@ -24,8 +25,8 @@ const getTodoInput = compose(map(map(head)), cgetDom('.todo-input'));
 
 const getTodo = compose(map(map(getTodoHtml)), map(map(prop('value'))), getTodoInput);
 
-const addTodo = compose(map(log), IO.runIO, getTodo);
+const addTodo = compose(map(log), runIO, getTodo);
 
 const main = compose(map(map(subscribe(addTodo, log))), buttonClickStream);
 
-main().runIO();
+runIO(main());
---
+++ app/utils/run-io.ts
@@ -0,0 +1,5 @@
+import {
+  IO
+} from 'ramda-fantasy';
+
+export default IO.runIO;
